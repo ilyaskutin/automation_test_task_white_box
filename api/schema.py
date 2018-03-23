13fdab56ee00c593118e765518f9c1ae748189a7
a.bogut <a.bogut@semrush.com>
2018-03-23 15:48:33 +0300

    add validation schema

---
+++ api/schema.py
@@ -0,0 +1,20 @@
+import re
+
+from voluptuous import (All, Length)
+from voluptuous.error import ValueInvalid
+from voluptuous.schema_builder import message
+
+
+@message('expected an Temperature', cls=ValueInvalid)
+def Temperature(v):
+    TEMPERATURE_REGEX = re.compile(
+        r'^[-+]?\d{1,10}(\.\d*)?\w?$',  # work with float
+        re.IGNORECASE
+    )
+
+    if not (TEMPERATURE_REGEX.match(v)):
+        raise ValueInvalid('Invalid temperature {}'.format(v))
+    return v
+
+
+temperature_validator = All(Temperature(), Length(max=10))
